55fbfa150ee194734c72c2bb75ec3ed5d0674e62
Charney Kaye <charney@outright.io>
2016-01-03 17:27:49 -0500

    add commented-out code from SDL experiments

--- mixer.go
+++ mixer.go
@@ -67,3 +67,52 @@ func AudioCallback(userdata unsafe.Pointer, stream *C.Uint8, length C.int) {
 	}
 	fmt.Printf("AudioCallback length %d\n", n)
 }
+
+
+
+/*
+
+// read audio file
+
+func ReadAudio() {
+
+	file := "assets/sounds/percussion/808/kick1.wav"
+
+	data, spec := sdl.LoadWAV(file, &sdl.AudioSpec{})
+
+	log.WithFields(log.Fields{
+		"spec":   spec,
+	}).Info("Loaded")
+
+	for n := 0; n < len(data); n += 2 {
+		StoreSample(data[n:n+2])
+	}
+}
+
+func StoreSample(s []byte) {
+	storedAudio = append(storedAudio, C.Uint16(int32(s[0]) + int32(s[1])<<8))
+}
+
+var storedAudio []C.Uint16
+var	(
+	defaultSample = uint16(0xFFFF)
+	defaultAudio = C.Uint16(defaultSample)
+)
+
+//export AudioCallback
+func AudioCallback(userdata unsafe.Pointer, stream *C.Uint16, length C.int) {
+	n := int(length)
+	hdr := reflect.SliceHeader{Data: uintptr(unsafe.Pointer(stream)), Len: n, Cap: n}
+	buf := *(*[]C.Uint16)(unsafe.Pointer(&hdr))
+
+	for i := 0; i < n; i += 1 {
+		if i < len(storedAudio) {
+			buf[i] = storedAudio[i]
+		} else {
+			buf[i] = defaultAudio
+		}
+	}
+	fmt.Printf("AudioCallback length %d\n", n)
+}
+
+*/
