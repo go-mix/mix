b6d431dc780806ef792545cca66c4541144a63df
Charney Kaye <charney@outright.io>
2016-01-03 13:54:36 -0500

    in order to export C callback function, make use of singleton

--- mixer.go
+++ mixer.go
@@ -1,33 +1,70 @@
 /** Copyright 2015 Outright Mental, Inc. */
-package atomix
-/* in-buffer mixing when timing is known in advance (e.g. music) */
+package atomix // is for sequence mixing
 
+/*
+#include <stdio.h>
+#include <stdint.h>
+typedef unsigned char Uint8;
+extern uint8_t* Sha256data(uint8_t* p0, int p1);
+void AudioCallback(void *userdata, Uint8 *stream, int len);
+*/
+import "C"
 import (
-  // log "github.com/Sirupsen/logrus"
+	"fmt"
+	"reflect"
+	"unsafe"
+	"sync"
+	// log "github.com/Sirupsen/logrus"
 	"github.com/veandco/go-sdl2/sdl"
 )
 
+
 type Mixer struct {
-	/* private */
-	spec *sdl.AudioSpec
+	// nothing yet
 }
 
-// New creates a new Mixer
-func New(spec sdl.AudioSpec) *Mixer {
-  if spec.Freq == 0 {
-    panic("Must specify Frequency")
-  } else if spec.Format == 0 {
-    panic("Must specify Format")
-  } else if spec.Channels == 0 {
-    panic("Must specify Channels")
-  } else if spec.Samples == 0 {
-    panic("Must specify Samples")
-  }
-	return &Mixer{
-		spec: &spec,
+func (m *Mixer) Initialize() {
+	// nothing yet
+}
+
+var instance *Mixer
+var once sync.Once
+
+func Instance() *Mixer {
+	once.Do(func() {
+		instance = &Mixer{}
+		instance.Initialize()
+	})
+	return instance
+}
+
+func Spec(spec *sdl.AudioSpec) *sdl.AudioSpec {
+	if spec.Freq == 0 {
+		panic("Must specify Frequency")
+	} else if spec.Format == 0 {
+		panic("Must specify Format")
+	} else if spec.Channels == 0 {
+		panic("Must specify Channels")
+	} else if spec.Samples == 0 {
+		panic("Must specify Samples")
 	}
+	spec.Callback = sdl.AudioCallback(C.AudioCallback)
+	return spec
 }
 
-func (m *Mixer) GetSpec() *sdl.AudioSpec {
-	return m.spec
+var	(
+	defaultSample = uint8(0xFF)
+	defaultAudio = C.Uint8(defaultSample)
+)
+
+//export AudioCallback
+func AudioCallback(userdata unsafe.Pointer, stream *C.Uint8, length C.int) {
+	n := int(length)
+	hdr := reflect.SliceHeader{Data: uintptr(unsafe.Pointer(stream)), Len: n, Cap: n}
+	buf := *(*[]C.Uint8)(unsafe.Pointer(&hdr))
+
+	for i := 0; i < n; i += 1 {
+		buf[i] = defaultAudio
+	}
+	fmt.Printf("AudioCallback length %d\n", n)
 }
--- mixer_test.go
+++ mixer_test.go
@@ -1,6 +1,5 @@
 /** Copyright 2015 Outright Mental, Inc. */
-package atomix
-/* in-buffer mixing when timing is known in advance (e.g. music) */
+package atomix // is for sequence mixing
 
 import (
 	"github.com/stretchr/testify/assert"
@@ -13,18 +12,17 @@ import (
 //
 
 func TestNewMixer(t *testing.T) {
-	mixer := New(sdl.AudioSpec{
+	assert.NotNil(t, Spec(&sdl.AudioSpec{
 		Freq:     44100,
 		Format:   sdl.AUDIO_U16,
 		Channels: 2,
 		Samples:  4096,
-	})
-	assert.NotNil(t, mixer.GetSpec())
+	}))
 }
 
 func TestNewMixerFailsWithoutProperAudioSpec(t *testing.T) {
 	assert.Panics(t, func() {
-		New(sdl.AudioSpec{})
+		Spec(&sdl.AudioSpec{})
 	})
 }
 
