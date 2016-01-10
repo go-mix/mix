08bfca6dafa0e4c2d854fd96607d3a087578f008
Charney Kaye <charney@outright.io>
2016-01-10 14:58:11 -0500

    API tests

---
+++ api_test.go
@@ -0,0 +1,40 @@
+/** Copyright 2015 Outright Mental, Inc. */
+package atomix // is for sequence mixing
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_API_Debug(t *testing.T) {
+	// TODO: Test API Debug
+}
+
+func Test_API_Configure(t *testing.T) {
+	// TODO: Test API Configure
+}
+
+func Test_API_Teardown(t *testing.T) {
+	// TODO: Test API Teardown
+}
+
+func Test_API_Spec(t *testing.T) {
+	// TODO: Test API Spec
+}
+
+func Test_API_SetFire(t *testing.T) {
+	// TODO: Test API SetFire
+}
+
+func Test_API_Start(t *testing.T) {
+	// TODO: Test API Start
+}
+
+func Test_API_StartAt(t *testing.T) {
+	// TODO: Test API StartAt
+}
+
+func Test_API_AudioCallback(t *testing.T) {
+	// TODO: Test API AudioCallback
+}
+
