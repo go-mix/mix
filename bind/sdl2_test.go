a268aa82960938919a9064001d0b01d577a3e4bc
Charney Kaye <charney@outright.io>
2016-01-29 16:45:55 -0500

    cgo not allowed in tests!

--- bind/sdl2_test.go
+++ bind/sdl2_test.go
@@ -3,13 +3,8 @@ package bind
 
 import (
 	"testing"
-	"unsafe"
 )
 
 func TestSDL2(t *testing.T) {
 	// TODO
-}
-
-func TestSDL2_AudioCallback(t *testing.T) {
-	AudioCallback(unsafe.Pointer{}, &uint8(5), int(25))
 }
